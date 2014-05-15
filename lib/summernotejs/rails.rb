c4efdcd0d9b281cabc41f9d518da5ead9338c394
mrgrt <mrgrahamethomson@gmail.com>
2014-05-15 15:26:20 +0100

    fix syntax error to inherit from rails engine

--- lib/summernotejs/rails.rb
+++ lib/summernotejs/rails.rb
@@ -2,6 +2,6 @@ require "summernotejs/rails/version"
 
 module Summernotejs
   module Rails
-    class Engine << ::Rails::Engine #declares gem as engine - causes rails to add its directories to the load path when gem is required.
+    class Engine < ::Rails::Engine #declares gem as engine - causes rails to add its directories to the load path when gem is required.
   end
 end
